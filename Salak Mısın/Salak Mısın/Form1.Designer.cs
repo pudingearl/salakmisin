41fb279d9100d24426913025b02f6f6268781c03
pudingearl <103273264+pudingearl@users.noreply.github.com>
2022-04-08 19:15:40 +0300

    Add files via upload

---
+++ Salak Mısın/Salak Mısın/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Salak_Mısın
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.evt = new System.Windows.Forms.Button();
+            this.hyr = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // evt
+            // 
+            this.evt.Location = new System.Drawing.Point(26, 151);
+            this.evt.Name = "evt";
+            this.evt.Size = new System.Drawing.Size(75, 23);
+            this.evt.TabIndex = 0;
+            this.evt.Text = "evet";
+            this.evt.UseVisualStyleBackColor = true;
+            this.evt.Click += new System.EventHandler(this.evt_Click);
+            // 
+            // hyr
+            // 
+            this.hyr.Location = new System.Drawing.Point(247, 151);
+            this.hyr.Name = "hyr";
+            this.hyr.Size = new System.Drawing.Size(75, 23);
+            this.hyr.TabIndex = 1;
+            this.hyr.Text = "hayır";
+            this.hyr.UseVisualStyleBackColor = true;
+            this.hyr.MouseEnter += new System.EventHandler(this.hyr_MouseEnter);
+            this.hyr.MouseHover += new System.EventHandler(this.hyr_MouseHover);
+            // 
+            // yazı
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(126, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Salak mısın?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 200);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hyr);
+            this.Controls.Add(this.evt);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button evt;
+        private System.Windows.Forms.Button hyr;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Salak Mısın/Salak Mısın/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Salak_Mısın
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            int fwidth = Screen.PrimaryScreen.WorkingArea.Width / 15 * 8;
+            int fheight = Screen.PrimaryScreen.WorkingArea.Height / 15 * 8;
+            int fvar = this.ClientSize.Height / 50 * 27;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(
+                Screen.PrimaryScreen.WorkingArea.Width - fwidth,
+                Screen.PrimaryScreen.WorkingArea.Height - fheight - fvar);
+        }
+
+
+        private void hyr_MouseEnter(object sender, EventArgs e)
+        {
+            var maxwidth = this.ClientSize.Width - hyr.ClientSize.Width;
+            var maxheight = this.ClientSize.Height - hyr.ClientSize.Height;
+            this.hyr.Location = new Point(
+                rnd.Next(99, maxwidth), rnd.Next(75, maxheight));
+        }
+
+        private void hyr_MouseHover(object sender, EventArgs e)
+        {
+            var maxwidth = this.ClientSize.Width - hyr.ClientSize.Width;
+            var maxheight = this.ClientSize.Height - hyr.ClientSize.Height;
+            this.hyr.Location = new Point(
+                rnd.Next(99, maxwidth), rnd.Next(75, maxheight));
+        }
+
+        private void evt_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("biliyordum :3");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.ClientSize = new Size(rnd.Next(350, 555), this.ClientSize.Height);
+            e.Cancel = true;
+        }
+    }
+}
